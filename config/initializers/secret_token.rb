e40e05f7207802fd58a2e710e231a9d2dfdaf76e
Red <redihiphop@gmail.com>
2014-04-13 04:35:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hh::Application.config.secret_key_base = 'c6c6f3f34d4daf24295888462b8421de4078c56a0bf11bf79e9f7dbbd484d4ef60a04176a23e2b4205df630109afbc86037ed2dd7d535752d9316d4f1a63a1f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hh::Application.config.session_store :cookie_store, key: '_hh_session'
